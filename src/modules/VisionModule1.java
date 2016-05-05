8a4456cbf9d98939723e5e3b66c747dddc5e9f7c
elc1798 <subliminalmau5@gmail.com>
2016-05-05 12:28:47 -0400

    Add drawing a tracker

--- src/modules/VisionModule1.java
+++ src/modules/VisionModule1.java
@@ -17,12 +17,29 @@ import gui.IntegerSliderVariable;
 import vision.VisionModule;
 
 public class VisionModule1 extends VisionModule {
+
     public IntegerSliderVariable minHue = new IntegerSliderVariable("Min Hue", 0, 0, 255);
     public IntegerSliderVariable maxHue = new IntegerSliderVariable("Max Hue", 0, 0, 255);
     public IntegerSliderVariable minSat = new IntegerSliderVariable("Min Saturation", 0, 0, 255);
     public IntegerSliderVariable maxSat = new IntegerSliderVariable("Max Saturation", 00, 0, 255);
     public IntegerSliderVariable minVal = new IntegerSliderVariable("Min Value", 254, 0, 255);
     public IntegerSliderVariable maxVal = new IntegerSliderVariable("Max Value", 255, 0, 255);
+
+    private double ghostX = Double.MAX_VALUE;
+    private double ghostY = Double.MAX_VALUE;
+    private double lastX = Double.MAX_VALUE;
+    private double lastY = Double.MAX_VALUE;
+    private double threshold = 10.0;
+    private Mat tracker = null;
+
+    public void drawTracker() {
+        if (Math.abs(lastX - ghostX) < threshold || Math.abs(lastY - ghostY) < threshold) {
+            Imgproc.line(tracker, new Point(ghostX, ghostY), new Point(lastX, lastY), new Scalar(255, 0, 0), 2);
+        }
+        ghostX = lastX;
+        ghostY = lastY;
+        postImage(tracker, "Pathing");
+    }
 
     public void drawBoundingRectangles(ArrayList<MatOfPoint> contours, Mat base) {
         double largestArea = 0.0;
@@ -41,7 +58,6 @@ public class VisionModule1 extends VisionModule {
 
         if (largestRect == null) return;
 
-        // Draw this bounding rectangle onto `drawn`
         Point[] points = new Point[4];
         largestRect.points(points);
         for (int line = 0; line < 4; line++) {
@@ -50,9 +66,12 @@ public class VisionModule1 extends VisionModule {
         Imgproc.circle(base, largestRect.center, 1, new Scalar(0, 0, 255), 2);
 
         double[] vector = new double[3];
-        vector[0] = largestRect.center.x - base.width() / 2.0;
-        vector[1] = largestRect.center.y - base.height() / 2.0;
+        vector[0] = largestRect.center.x;
+        vector[1] = largestRect.center.y;
         vector[2] = largestRect.angle;
+
+        lastX = vector[0];
+        lastY = vector[1];
 
         double h = base.height();
         Point textPos = new Point(0, h - 30);
@@ -67,49 +86,41 @@ public class VisionModule1 extends VisionModule {
     public void run(Mat frame) {
         postImage(frame, "Master");
 
-        // blur
+        if (tracker == null) {
+            tracker = frame.clone();
+            tracker.setTo(new Scalar(255, 255, 255));
+        }
+
         Mat blurred = new Mat();
         Imgproc.medianBlur(frame, blurred, 5);
-        postImage(blurred, "Blurred");
 
-        // split into hsv channels
         Mat hsv = new Mat();
         Imgproc.cvtColor(blurred, hsv, Imgproc.COLOR_BGR2HSV);
         ArrayList<Mat> channels = new ArrayList<Mat>();
         Core.split(hsv, channels);
 
-        // postImage(channels.get(0), "Unthresholded Hue");
-        // postImage(channels.get(1), "Unthresholded Saturation");
-        // postImage(channels.get(2), "Unthresholded Value");
-
-        // hue is the 0th index in hsv
         Core.inRange(channels.get(0), new Scalar(minHue.value()), new Scalar(maxHue.value()), channels.get(0));
-        // postImage(channels.get(0), "Hue thresholded");
-        // saturation is the 1st index in hsv
         Core.inRange(channels.get(1), new Scalar(minSat.value()), new Scalar(maxSat.value()), channels.get(1));
-        // postImage(channels.get(1), "Saturation thresholded");
-        // saturation is the 1st index in hsv
         Core.inRange(channels.get(2), new Scalar(minVal.value()), new Scalar(maxVal.value()), channels.get(2));
-        // postImage(channels.get(2), "Value thresholded");
 
-        // combine thresholded images
         Mat thresholdedImage = new Mat();
         Core.bitwise_and(channels.get(0), channels.get(1), thresholdedImage);
         Core.bitwise_and(thresholdedImage, channels.get(2), thresholdedImage);
-        // postImage(thresholdedImage, "Final threshed");
 
-        // erode and dilate
         Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
         Imgproc.erode(thresholdedImage, thresholdedImage, kernel);
         Imgproc.dilate(thresholdedImage, thresholdedImage, kernel);
+
         postImage(thresholdedImage, "Erode and dilate");
 
-        // find all outer contours
         ArrayList<MatOfPoint> contours = new ArrayList<>();
         Imgproc.findContours(thresholdedImage, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         Mat frameCopy = frame.clone();
         drawBoundingRectangles(contours, frameCopy);
+
         postImage(frameCopy, "Contours");
         postTag("Contours", "contourCount", "Contours: " + contours.size());
+
+        drawTracker();
     }
 }
