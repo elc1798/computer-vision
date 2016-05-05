541fc9d8f834bd4565d4dec1810710e880473d03
elc1798 <subliminalmau5@gmail.com>
2016-05-05 12:08:35 -0400

    Add simple switch mechanism for demoing modules

--- src/modules/VisionModuleSuite.java
+++ src/modules/VisionModuleSuite.java
@@ -5,15 +5,28 @@ import vision.ModuleRunner;
 
 public class VisionModuleSuite {
 
+    private static int demoID = 0;
+
     /**
      * Add any mappings here from capture sources to vision modules
      * Available capture sources:
-     *   - DeviceCaptureSource
-     *   - VideoCaptureSource
-     *   - ImageCaptureSource
+     * - DeviceCaptureSource
+     * - VideoCaptureSource
+     * - ImageCaptureSource
      */
+
     static {
         String imageDirectory = VisionModuleSuite.class.getResource("").getPath() + "../../images/";
-        ModuleRunner.addMapping(new DeviceCaptureSource(0), new FaceDetectModule());
+        switch (demoID) {
+            case 0:
+                ModuleRunner.addMapping(new DeviceCaptureSource(0), new VisionModule1());
+                break;
+            case 1:
+                ModuleRunner.addMapping(new DeviceCaptureSource(0), new FaceDetectModule());
+                break;
+            case 2:
+            default:
+                break;
+        }
     }
 }
